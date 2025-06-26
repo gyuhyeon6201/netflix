b4c6c9f0ca7acbf2d932555e3a657de6021a6d95
gyuhyeon6201 <rbgus303@gmail.com>
2025-06-26 12:40:02 +0900

    create netfilx project 0623-26 v3

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
 import "./App.scss";
 import MainLayout from "./components/MainLayout";
 import Main from "./components/Main";
 import Login from "./components/Login";
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<Main />}/>
-          <Route path="login" element={<Login />}/>
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        // <BrowserRouter>
+        <HashRouter>
+            <Routes>
+                <Route path="/" element={<MainLayout />}>
+                    <Route index element={<Main />} />
+                    <Route path="login" element={<Login />} />
+                </Route>
+            </Routes>
+        </HashRouter>
+        // </BrowserRouter>
+    );
 };
 
 export default App;
